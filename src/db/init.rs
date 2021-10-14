a067d8603a6464b97fd19856e2a3861a3ab0f6cf
Sunken Ahn <devsanso@outlook.com>
2021-10-15 01:39:39 +0900

    remove id col, and add test method

--- src/db/init.rs
+++ src/db/init.rs
@@ -1,17 +1,31 @@
 use rusqlite::{Connection,Error};
 
 
-const INIT_TABLE : &str = "CREATE TABLE word (
-    id                  INTEGER AUTO INCREMENT,
-    chapter        INTEGER
+const INIT_TABLE : &str = "CREATE TABLE word(
+    chapter        INTEGER,
     page             INTEGER,
     origin_text   VARCHAR(256),
-    trans_text    VARCHAR(256)
-    PRIMARY KEY(id),
-    UNQIUE(origin_text)
-)";
+    trans_text    VARCHAR(256),
+    UNIQUE(origin_text)
+);";
 
 
 pub fn  init_db(conn : &Connection) -> Result<usize,Error> {
     conn.execute(INIT_TABLE ,[])
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use rusqlite;
+    use rusqlite::{Connection};
+    #[test]
+    fn test_init() -> rusqlite::Result<()> {
+        let conn = Connection::open_in_memory()?;
+        super::init_db(&conn)?;
+    
+        Ok(())
+        
+    }
 }
