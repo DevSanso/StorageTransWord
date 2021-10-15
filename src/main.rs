92964cf5bb82f9d5f7b2f82d40f752dfc7daf981
Sunken Ahn <devsanso@outlook.com>
2021-10-15 14:00:52 +0900

    will write

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
+use std::include_str;
+use std::path::Path;
+use std::fs::read_to_string;
+use rusqlite::Connection;
+
 mod driver;
 mod db;
 
 use db::init::init_db;
+const DBNAME : &str = "words.db";
 
-fn main() {
+
+fn read_config() -> String {
+    let p = Path::new("./").join("./driver_config.json");
+    read_to_string(p).unwrap()
+}
+
+fn main()  { 
+    let db_path = Path::new("./").join(DBNAME);
+    let driver_config = read_config();
+    let conn = Connection::open(db_path);
+
+    let trans_driver = driver::facory(driver_config, driver::Drivers::Papago);
+
+    loop {
+        
+    }
     
-    println!("Hello, world!");
 }
