344f2aa967d0484b6f26bf531460b39ed25bbe20
Sunken Ahn <devsanso@outlook.com>
2021-10-23 16:00:29 +0900

    input_result error type &str -> String, parsing i32 fix del \n charecter in read_line string, and input action number 4 work, buf 1,2,3 not working

--- src/console/view/word.rs
+++ src/console/view/word.rs
@@ -26,7 +26,7 @@ pub struct WordView {
     is_close : bool,
     action : Action,
 
-    input_result : Result<Option<Word>,&'static str>,
+    input_result : Result<Option<Word>,String>,
     num_temp : Option<i32>
 
 }
@@ -43,7 +43,7 @@ impl WordView {
             input_result : Ok(None),
             num_temp : None
         };
-        w.update();
+        w.update_word_list();
         w
     }
 
@@ -52,6 +52,7 @@ impl WordView {
         let mut temp_buf = String::new();
         print!("페이지를 입력하세요(숫자만) : ");
         io::stdin().lock().read_line(&mut temp_buf);
+        temp_buf = temp_buf.replace("\n", "");
         let casting = temp_buf.parse::<i32>();
         if casting.is_err() {
             return Err("숫자만 입력하세요");
@@ -65,20 +66,22 @@ impl WordView {
     fn input_num_temp(&mut self) {
         let mut buf = String::new();
         io::stdin().lock().read_line(&mut buf);
+        buf = buf.replace("\n", "");
         let casting = buf.parse::<i32>();
         if casting.is_err() {
-            self.input_result = Err("숫자만 입력하세요");
+            self.input_result = Err(String::from("숫자만 입력하세요"));
+            return;
         }
         self.num_temp = Some(casting.unwrap());
     }
-    fn insert_word(&self,w : Word) -> Result<(),&'static str>{
-
+    fn insert_word(&self, w : Word) -> Result<(),&'static str>{
+        let mut temp = w;
         let trans_d = Var::get_driver_as_ref();
-        let res = trans_d.trans(w.origin_text.clone());
+        let res = trans_d.trans(temp.origin_text.clone());
 
-        w.trans_text = res.data;
+        temp.trans_text = res.data;
         let conn = Var::get_db_conn_as_mut_ref();
-        if  Word::push(&conn,w).is_err() {
+        if  Word::push(&conn,temp).is_err() {
             return Err("database push error");
         }
         Ok(())
@@ -102,19 +105,23 @@ impl WordView {
         let conn = Var::get_db_conn_as_mut_ref();
         let data = Word::get_word_in_book_and_chapter(conn,self.book_id,self.chapter);
         if data.is_err() {
-            let errMsg : &str = format!("{}",data.err().unwrap());
-            self.input_result = Err(errMsg);
+            let errMsg : String = format!("{}",data.err().unwrap()).to_string();
+            self.input_result = Err(errMsg.clone());
             return;
         }
 
-        self.list = Some(WordList::new(self.book_name,self.chapter,data.unwrap()));
+        self.list = Some(WordList::new(self.book_name.clone(),self.chapter,data.unwrap()));
     }
     fn sub_update_next_action(&mut self) {
+        if self.num_temp.is_none() {
+            return;
+        }
         let num = self.num_temp.unwrap();
         if num == 1 {self.action = Action::Insert;}
         else if num == 2 {self.action = Action::Delete;}
         else if num == 3 {self.action = Action::ChangeChapter;}
-        else if num == 4 {self.action == Action::Exit;}
+        else if num == 4 {self.action = Action::Exit;}
+        else {self.action = Action::Nothing;}
     }
        
 
@@ -126,7 +133,7 @@ impl View for WordView {
         println!("{}",self.list.as_ref().unwrap());
         println!("");
         if self.action == Action::Nothing {
-            print!("입력 : 1(단어 추가), 2(단어 삭제), 3(챕터 변경),4(돌아가기) :");
+            print!("입력 : 1(단어 추가), 2(단어 삭제), 3(챕터 변경),4(돌아가기) :\n");
         }
 
         Ok(())
@@ -135,7 +142,7 @@ impl View for WordView {
         if self.action == Action::Insert && self.action == Action::Delete{
             self.input_result = match self.stdin_read_and_init_word_struct_data() {
                 Ok(ok) => Ok(Some(ok)),
-                Err(err) => Err(err)
+                Err(err) => Err(String::from(err))
             };
             
             return Ok(());
@@ -145,34 +152,31 @@ impl View for WordView {
         Ok(())
     }
     fn update(&mut self) -> io::Result<()> {
-        if self.action == Action::Nothing {
-            self.sub_update_next_action();
-            return Ok(());
-        }
-        else if self.action == Action::Exit {
+        self.sub_update_next_action();
+
+        if self.action == Action::Exit {
             self.is_close = true;
-            self.action == Action::Nothing;
             return Ok(());
         }
         else if self.action == Action::ChangeChapter {
             self.chapter = self.num_temp.unwrap();
-            self.action == Action::Nothing;
+            self.action = Action::Nothing;
             return Ok(());
         }
 
     
 
-        let res = self.input_result;
+        let res = self.input_result.clone();
         if res.is_err() {
             return Ok(());
         }
         
         if self.action == Action::Insert {
-            let w = self.input_result.unwrap().unwrap();
+            let mut w = res.unwrap().unwrap();
             self.insert_word(w);
         }
-        if self.action == Action::Delete {
-            let w = self.input_result.unwrap().unwrap();
+        else if self.action == Action::Delete {
+            let w = res.unwrap().unwrap();
             self.delete_word(w);
         }
 
@@ -186,9 +190,38 @@ impl View for WordView {
     }
     fn next<'parent>(&self) -> Option<Box<dyn View + 'parent>> {
         if self.input_result.is_err() {
-            let msg = self.input_result.err().unwrap();
-            return Some(Box::new(ErrorStringView::new(msg)));
+            let msg = self.input_result.clone().err().unwrap();
+            return Some(Box::new(ErrorStringView::new(
+                msg
+            )));
         }
         None
+    }
+}
+#[cfg(test)]
+mod tests {
+    use std::io;
+    use std::io::Read;
+    use rusqlite::Connection;
+    use super::super::View;
+    use super::WordView;
+    use crate::db::init::init_db;
+    use crate::db::table::Book;
+    use crate::var::Var;
+    #[test]
+    fn word_view_test() {
+        Var::test_init();
+        let mut v :Box<dyn View> = Box::new(WordView::new(String::from("hello"),1,1));
+        let c = Var::get_db_conn_as_mut_ref();
+        Book::push(&c,String::from("hello"));
+        while v.is_more_run() {
+
+            println!("running");
+            v.exec();
+            let m = v.next();
+            if m.is_some() {
+                m.unwrap().exec();
+            }
+        }
     }
 }
